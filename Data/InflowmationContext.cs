491267c4289384e264707ade0f9fde0f6cd74aba
Ashkan Taravati <Ashkan Taravati@TRUSTYTERRIER>
2023-05-25 18:56:38 +0330

    Add project files.

---
+++ Data/InflowmationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Inflowmation.Models;
+
+namespace Inflowmation.Data
+{
+    public class InflowmationContext : DbContext
+    {
+        public InflowmationContext (DbContextOptions<InflowmationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Inflowmation.Models.Request> Request { get; set; } = default!;
+    }
+}
---
+++ Models/Request.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inflowmation.Models
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+
+        [MaxLength(400)]
+        public string? Descriptions { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Employee CreatedBy { get; set; }
+
+
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+    }
+}
